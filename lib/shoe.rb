07c46caadcfdd4e0478e9ae4c9ce053354a27da9
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-06-07 22:59:34 -0600

    commit tree 60e6a600705b1f5636be2ae08c293efb6b7d7020

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand, color=nil, size=nil, material=nil, condition=nil)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = 'new'
+    end
+ end
